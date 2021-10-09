9f6a84779e0830d31c86ecc8b92f155adb62099e
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2021-10-09 23:11:10 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -90,8 +90,8 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('Moe Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('Search again🔍', switch_inline_query_current_chat=query),
+            InlineKeyboardButton('Join Group🍿', url='https://t.me/cinemakodathi')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
