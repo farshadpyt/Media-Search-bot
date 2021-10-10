d719d84425f8485a9be56b22b89317a61d43c8aa
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2021-10-10 07:37:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -66,8 +66,8 @@ async def start(bot, cmd):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('Search again', switch_inline_query_current_chat=''),
-                        InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+                        InlineKeyboardButton('Search again🔍', switch_inline_query_current_chat=''),
+                        InlineKeyboardButton('Group🍿', url='https://t.me/cinemakodathi')
                     ]
                     ]
                 await bot.send_cached_media(
@@ -99,11 +99,12 @@ async def start(bot, cmd):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Other Bots", url="https://t.me/subin_works/122")
+                        InlineKeyboardButton("Search Here🔍", switch_inline_query_current_chat=''),
+                        InlineKeyboardButton("Join Group🍿", url="https://t.me/cinemakodathi")
                     ],
                     [
-                        InlineKeyboardButton("About", callback_data="about")
+                        InlineKeyboardButton("About😎", callback_data="about")
+                        InlineKeyboardButton("Mydev🧑‍💻", url="https://t.me/farshadck")
                     ]
                 ]
             )
@@ -192,8 +193,8 @@ async def delete(bot, message):
 async def bot_info(bot, message):
     buttons = [
         [
-            InlineKeyboardButton('Update Channel', url='https://t.me/subin_works'),
-            InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
+            InlineKeyboardButton('💢Update Channel💢', url='https://t.me/botupdatechannelfarshad'),
+            InlineKeyboardButton('😂Source Code😂', url='https://t.me/botupdatechannelfarshad')
         ]
         ]
     await message.reply(text="Language : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
