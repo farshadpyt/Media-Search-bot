db3e49908eb0c6030b5c9aae114a296a80661440
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2021-10-15 20:21:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -119,7 +119,17 @@ async def group(client, message):
             BOT["username"]=nyva
         files = await get_filter_results(query=search)
         if files:
-            for file in files: file_id = file.file_id file_name = file.file_name file_size = get_size(file.file_size) file_link = f"https://telegram.dog/{nyva}?start=subinps_-_-_-_{file_id}" btn.append( [ InlineKeyboardButton(text=f"{file_name}", url=f"{file_link}"), InlineKeyboardButton(text=f"{file_size}", url=f"{file_link}") ] )
+            for file in files: 
+                file_id = file.file_id 
+                file_name = file.file_name 
+                file_size = get_size(file.file_size) 
+                file_link = f"https://telegram.dog/{nyva}?start=subinps_-_-_-_{file_id}" 
+                btn.append( 
+                    [ 
+                      InlineKeyboardButton(text=f"{file_name}", url=f"{file_link}"), 
+                      InlineKeyboardButton(text=f"{file_size}", url=f"{file_link}") 
+                    ] 
+                )
         else:
             return
         if not btn:
