fd99c23c072c4b041a97383edeb29682ed6c3d7f
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2021-11-02 14:44:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -139,21 +139,7 @@ async def channel_info(bot, message):
         with open(file, 'w') as f:
             f.write(text)
         await message.reply_document(file)
-        os.remove(file)
-@Client.on_message(filters.command('users') & filters.user(ADMINS))
-async def list_users(bot, message):
-    # https://t.me/GetTGLink/4184
-    raju = await message.reply('Getting List Of Users')
-    users = await db.get_all_users()
-    out = "Users Saved In DB Are:\n\n"
-    async for user in users:
-        out += f"<a href=tg://user?id={user['id']}>{user['name']}</a>\n"
-    try:
-        await raju.edit_text(out)
-    except MessageTooLong:
-        with open('users.txt', 'w+') as outfile:
-            outfile.write(out)
-        await message.reply_document('users.txt', caption="List Of Users")       
+        os.remove(file)       
 
 
 @Client.on_message(filters.command('total') & filters.user(ADMINS))
